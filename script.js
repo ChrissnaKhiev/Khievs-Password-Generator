55983873b746a182261fe2d7e09627b797bc0b74
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-14 12:10:47 -0400

    added user input prompts

--- script.js
+++ script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var charNum = prompt("Enter number of characters: (8-128)");
+  var lowC = confirm("Include Lowercase?");
+  var upC = confirm("Include Uppercase?");
+  var numbs = confirm("Include Numbers?");
+  var speC = confirm("Include Special Characters?")
+
+  charNum;
+  lowC;
+  upC;
+  numbs;
+  speC;
+
+  console.log(charNum);
+  console.log(lowC);
+  console.log(upC);
+  console.log(numbs);
+  console.log(speC);
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

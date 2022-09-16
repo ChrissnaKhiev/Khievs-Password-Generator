9a75180cb972d9d3125ca935cf04d7c17a9c39d4
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-16 04:42:54 -0400

    finished code allnighter edition

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Password-Generator
+This is a random password generator.
+
+## Description
+
+This password generator will generate you a password based on the parameters the user inputs like length, lowercase, uppercase, numbers, and symbols.
+
+## Installation
+
+N/A
+
+## Usage
+
+User will be able to generate a password in which they can use for their profiles for different applications.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -11,23 +11,66 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = lowerCase.toUpperCase();
+  var numbers = "0123456789";
+  var specialChar = "!#$%&*^@";
+
+  var passString = "";
+
   var charNum = prompt("Enter number of characters: (8-128)");
+
   var lowC = confirm("Include Lowercase?");
+
+  if (lowC === true) {
+    passString += lowerCase;
+  }
+
+  //check1
+  console.log(passString.length);
+  console.log(passString);
+  
   var upC = confirm("Include Uppercase?");
+
+  if (upC === true) {
+    passString += upperCase;
+  }
+
+  //check2
+  console.log(passString.length);
+  console.log(passString);
+
   var numbs = confirm("Include Numbers?");
-  var speC = confirm("Include Special Characters?")
 
-  charNum;
-  lowC;
-  upC;
-  numbs;
-  speC;
+  if (numbs === true) {
+    passString += numbers;
+  }
 
-  console.log(charNum);
-  console.log(lowC);
-  console.log(upC);
-  console.log(numbs);
-  console.log(speC);
+  //check3
+  console.log(passString.length);
+  console.log(passString);
+
+  var speC = confirm("Include Special Characters? (!#$%&*^@)")
+
+  if (speC === true) {
+    passString += specialChar;
+  }
+
+  //check4
+  console.log(passString.length);
+  console.log(passString);
+  
+  var final = "";
+
+  for (var i = 0; i < charNum; i++) {
+    
+    var randomNumber = Math.floor(Math.random() * passString.length);
+    final += passString.substring(randomNumber, randomNumber +1);
+    console.log(final);//process
+    }
+    return final;
+
+  
 }
 
 // Add event listener to generate button
